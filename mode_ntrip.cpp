dcff04136ad5959b232ba3363fb3b60c03f1113e
Aleksandr Tolkov <mail@radiofisik.ru>
2025-05-25 13:27:38 +0200

    switch to ntrp make led blink

--- mode_ntrip.cpp
+++ mode_ntrip.cpp
@@ -8,11 +8,29 @@
 
 extern Settings settings;
 
-#define UART2_RX 16 // Set according to physical wiring
+#define UART2_RX 16  // Set according to physical wiring
 #define UART2_TX 17
 
 
+#define LED_PIN 2
+
+
+void blinkTask(void *pvParameters) {
+  pinMode(LED_PIN, OUTPUT);
+  for (;;) {
+    digitalWrite(LED_PIN, HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1000 ms == 1s
+    digitalWrite(LED_PIN, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  // Optionally vTaskDelete(NULL);
+}
+
 void runNtripServerMode() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  delay(100);  // Allow time for Serial to initialize
+
   // Start WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(settings.ssid, settings.wifiPassword);
@@ -26,6 +44,10 @@ void runNtripServerMode() {
     }
   }
 
+  // Once WiFi is connected, print IP address to serial
+  Serial.print("WiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
   GNSS.begin(settings.gnssBaud, SERIAL_8N1, UART2_RX, UART2_TX);
 
   // NTRIPServer ntrip;
@@ -35,11 +57,25 @@ void runNtripServerMode() {
   //             settings.ntripUser,
   //             settings.ntripPassword);
 
+  xTaskCreatePinnedToCore(
+    blinkTask,    // Task code
+    "BlinkTask",  // name
+    2048,         // stack size (words, not bytes!)
+    NULL,         // params
+    1,            // priority
+    NULL,         // task handle
+    1);           // core (0 or 1)
+
   for (;;) {
+
+   
+
+
+
     // Forward GNSS output to NTRIP server
-    while (GNSS.available()) {
-      // ntrip.send(GNSS.read());
-    }
+    //   while (GNSS.available()) {
+    //   // ntrip.send(GNSS.read());
+    // }
     // ntrip.handleClient(); // allow NTRIP server to clear clients/make connections
     delay(1);
   }
