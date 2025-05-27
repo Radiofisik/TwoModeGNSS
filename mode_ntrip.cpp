149aa69dd1405e795e859ea3fae1a11b425e7c6e
Aleksandr Tolkov <mail@radiofisik.ru>
2025-05-27 22:30:27 +0200

    ntrip caster is working

--- mode_ntrip.cpp
+++ mode_ntrip.cpp
@@ -10,19 +10,140 @@
 
 extern Settings settings;
 
-NtripSourceClient ntrip;
+WiFiClient ntripCaster;
 
-// You may tune this chunk size and/or add message detection for RTCM
-#define RTCM_BUF_SIZE 256
-uint8_t rtcmBuf[RTCM_BUF_SIZE];
-size_t rtcmBufPos = 0;
+long lastSentRTCM_ms = 0;           //Time of last data pushed to socket
+int maxTimeBeforeHangup_ms = 10000; //If we fail to get a complete RTCM frame after 10s, then disconnect from caster
+uint32_t serverBytesSent = 0; //Just a running total
+long lastReport_ms = 0;       //Time of last report of bytes sent
+
+void beginServer()
+{
+  Serial.println("Begin transmitting to caster. Press any key to stop");
+  delay(10); //Wait for any serial to arrive
+  while (Serial.available())
+    Serial.read(); //Flush
+
+  while (Serial.available() == 0)
+  {
+    //Connect if we are not already
+    if (ntripCaster.connected() == false)
+    {
+      Serial.printf("Opening socket to %s\n", settings.casterHost);
+
+      if (ntripCaster.connect(settings.casterHost, settings.casterPort) == true) //Attempt connection
+      {
+        Serial.printf("Connected to %s:%d\n", settings.casterHost, settings.casterPort);
+
+        const int SERVER_BUFFER_SIZE = 512;
+        char serverRequest[SERVER_BUFFER_SIZE];
+
+        snprintf(serverRequest,
+                 SERVER_BUFFER_SIZE,
+                 "SOURCE %s /%s\r\nSource-Agent: NTRIP SparkFun UM980 Server v1.0\r\n\r\n",
+                 settings.ntripPassword, settings.mountpoint);
+
+        Serial.println(F("Sending server request:"));
+        Serial.println(serverRequest);
+        ntripCaster.write(serverRequest, strlen(serverRequest));
+
+        //Wait for response
+        unsigned long timeout = millis();
+        while (ntripCaster.available() == 0)
+        {
+          if (millis() - timeout > 5000)
+          {
+            Serial.println("Caster timed out!");
+            ntripCaster.stop();
+            return;
+          }
+          delay(10);
+        }
+
+        //Check reply
+        bool connectionSuccess = false;
+        char response[512];
+        int responseSpot = 0;
+        while (ntripCaster.available())
+        {
+          response[responseSpot++] = ntripCaster.read();
+          if (strstr(response, "200") != nullptr) //Look for 'ICY 200 OK'
+            connectionSuccess = true;
+          if (responseSpot == 512 - 1)
+            break;
+        }
+        response[responseSpot] = '\0';
+
+        if (connectionSuccess == false)
+        {
+          Serial.printf("Failed to connect to Caster: %s", response);
+          return;
+        }
+      } //End attempt to connect
+      else
+      {
+        Serial.println("Connection to host failed");
+        return;
+      }
+    } //End connected == false
+
+    if (ntripCaster.connected() == true)
+    {
+      delay(10);
+      while (Serial.available())
+        Serial.read(); //Flush any endlines or carriage returns
+
+      lastReport_ms = millis();
+      lastSentRTCM_ms = millis();
+
+      //This is the main sending loop. We scan for new data but processRTCM() is where the data actually gets sent out.
+      while (1)
+      {
+        if (Serial.available())
+          break;
+
+        //Write incoming RTCM to the NTRIP Caster
+        while (GNSS.available())
+        {
+          ntripCaster.write(GNSS.read()); //Send this byte to socket
+          serverBytesSent++;
+          lastSentRTCM_ms = millis();
+        }
+
+        //Close socket if we don't have new data for 10s
+        //RTK2Go will ban your IP address if you abuse it. See http://www.rtk2go.com/how-to-get-your-ip-banned/
+        //So let's not leave the socket open/hanging without data
+        if (millis() - lastSentRTCM_ms > maxTimeBeforeHangup_ms)
+        {
+          Serial.println("RTCM timeout. Disconnecting...");
+          ntripCaster.stop();
+          return;
+        }
+
+        delay(10);
+
+        //Report some statistics every 250
+        if (millis() - lastReport_ms > 250)
+        {
+          lastReport_ms += 250;
+          Serial.printf("Total sent: %d\n", serverBytesSent);
+        }
+      }
+    }
+
+    delay(10);
+  }
+}
 
 void runNtripServerMode() {
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
 
     Serial.begin(115200);
-    delay(100);
+    delay(250);
+
+    Serial.println();
+    Serial.println("ntrip server mode");
 
     // Connect to WiFi
     WiFi.mode(WIFI_STA);
@@ -43,52 +164,14 @@ void runNtripServerMode() {
     // Start GNSS UART
     GNSS.begin(settings.gnssBaud, SERIAL_8N1, UART2_RX, UART2_TX);
 
-    // Connect to NTRIP Caster as source
-    bool connected = ntrip.connect(
-        settings.casterHost,
-        settings.casterPort,
-        "RTKLIB/2.4",      // Agent name (can adjust if needed)
-        settings.mountpoint,
-        settings.ntripPassword // pass as "password" (username is ignored)
-    );
+    //Clear any serial characters from the buffer
+    while (Serial.available())
+        Serial.read();
 
-    if (!connected) {
-        Serial.println("[NTRIP] Initial connection failed. Rebooting...");
-        delay(2000);
-        ESP.restart();
-    }
 
     // Main loop
     for (;;) {
-        // Buffer and send RTCM in blocks, with header each time
-        while (GNSS.available()) {
-            uint8_t b = GNSS.read();
-            rtcmBuf[rtcmBufPos++] = b;
-            if (rtcmBufPos == RTCM_BUF_SIZE) {
-                ntrip.sendHeader("RTKLIB/2.4", settings.mountpoint, settings.ntripPassword); // send header before block
-                ntrip.sendData(rtcmBuf, rtcmBufPos);
-                rtcmBufPos = 0;
-            }
-        }
-
-        // Flush buffer every 200ms, even if not full
-        static unsigned long lastFlush = 0;
-        if (rtcmBufPos > 0 && millis() - lastFlush > 200) {
-            ntrip.sendHeader("RTKLIB/2.4", settings.mountpoint, settings.ntripPassword);
-            ntrip.sendData(rtcmBuf, rtcmBufPos);
-            rtcmBufPos = 0;
-            lastFlush = millis();
-        }
-
-        ntrip.handle();
-
-        // Reconnect logic if dropped
-        if (!ntrip.isConnected()) {
-            Serial.println("[NTRIP] Disconnected, retrying in 5 seconds...");
-            delay(5000);
-            ESP.restart(); // or re-run the connect() logic, or break to outer loop
-        }
-
-        delay(1);
+        beginServer();
+        delay(1000);
     }
 }
