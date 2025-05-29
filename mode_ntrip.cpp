ecbda085b3a29477f8930cbb78f74e12ee73977a
Aleksandr Tolkov <mail@radiofisik.ru>
2025-05-29 08:12:32 +0200

    prevent hang up

--- mode_ntrip.cpp
+++ mode_ntrip.cpp
@@ -1,37 +1,35 @@
 #include "mode_ntrip.h"
 #include "settings.h"
-#include "ntrip.h"
 #include "gnss_uart.h"
 #include <WiFi.h>
 
 #define UART2_RX 16
 #define UART2_TX 17
 #define LED_PIN 2
+#define SERIAL_SIZE_RX 16384
 
 extern Settings settings;
 
 WiFiClient ntripCaster;
 
-long lastSentRTCM_ms = 0;           //Time of last data pushed to socket
-int maxTimeBeforeHangup_ms = 10000; //If we fail to get a complete RTCM frame after 10s, then disconnect from caster
-uint32_t serverBytesSent = 0; //Just a running total
-long lastReport_ms = 0;       //Time of last report of bytes sent
+long lastSentRTCM_ms = 0;            //Time of last data pushed to socket
+int maxTimeBeforeHangup_ms = 10000;  //If we fail to get a complete RTCM frame after 10s, then disconnect from caster
+uint32_t serverBytesSent = 0;        //Just a running total
+long lastReport_ms = 0;              //Time of last report of bytes sent
+long actualReportDelay = 0;
 
-void beginServer()
-{
+void beginServer() {
   Serial.println("Begin transmitting to caster. Press any key to stop");
-  delay(10); //Wait for any serial to arrive
+  delay(10);  //Wait for any serial to arrive
   while (Serial.available())
-    Serial.read(); //Flush
+    Serial.read();  //Flush
 
-  while (Serial.available() == 0)
-  {
+  while (Serial.available() == 0) {
     //Connect if we are not already
-    if (ntripCaster.connected() == false)
-    {
+    if (ntripCaster.connected() == false) {
       Serial.printf("Opening socket to %s\n", settings.casterHost);
 
-      if (ntripCaster.connect(settings.casterHost, settings.casterPort) == true) //Attempt connection
+      if (ntripCaster.connect(settings.casterHost, settings.casterPort) == true)  //Attempt connection
       {
         Serial.printf("Connected to %s:%d\n", settings.casterHost, settings.casterPort);
 
@@ -49,10 +47,8 @@ void beginServer()
 
         //Wait for response
         unsigned long timeout = millis();
-        while (ntripCaster.available() == 0)
-        {
-          if (millis() - timeout > 5000)
-          {
+        while (ntripCaster.available() == 0) {
+          if (millis() - timeout > 5000) {
             Serial.println("Caster timed out!");
             ntripCaster.stop();
             return;
@@ -64,48 +60,42 @@ void beginServer()
         bool connectionSuccess = false;
         char response[512];
         int responseSpot = 0;
-        while (ntripCaster.available())
-        {
+        while (ntripCaster.available()) {
           response[responseSpot++] = ntripCaster.read();
-          if (strstr(response, "200") != nullptr) //Look for 'ICY 200 OK'
+          if (strstr(response, "200") != nullptr)  //Look for 'ICY 200 OK'
             connectionSuccess = true;
           if (responseSpot == 512 - 1)
             break;
         }
         response[responseSpot] = '\0';
 
-        if (connectionSuccess == false)
-        {
+        if (connectionSuccess == false) {
           Serial.printf("Failed to connect to Caster: %s", response);
           return;
         }
-      } //End attempt to connect
-      else
-      {
+      }  //End attempt to connect
+      else {
         Serial.println("Connection to host failed");
         return;
       }
-    } //End connected == false
+    }  //End connected == false
 
-    if (ntripCaster.connected() == true)
-    {
+    if (ntripCaster.connected() == true) {
       delay(10);
       while (Serial.available())
-        Serial.read(); //Flush any endlines or carriage returns
+        Serial.read();  //Flush any endlines or carriage returns
 
       lastReport_ms = millis();
       lastSentRTCM_ms = millis();
 
       //This is the main sending loop. We scan for new data but processRTCM() is where the data actually gets sent out.
-      while (1)
-      {
+      while (1) {
         if (Serial.available())
           break;
 
         //Write incoming RTCM to the NTRIP Caster
-        while (GNSS.available())
-        {
-          ntripCaster.write(GNSS.read()); //Send this byte to socket
+        while (GNSS.available()) {
+          ntripCaster.write(GNSS.read());  //Send this byte to socket
           serverBytesSent++;
           lastSentRTCM_ms = millis();
         }
@@ -113,8 +103,7 @@ void beginServer()
         //Close socket if we don't have new data for 10s
         //RTK2Go will ban your IP address if you abuse it. See http://www.rtk2go.com/how-to-get-your-ip-banned/
         //So let's not leave the socket open/hanging without data
-        if (millis() - lastSentRTCM_ms > maxTimeBeforeHangup_ms)
-        {
+        if (millis() - lastSentRTCM_ms > maxTimeBeforeHangup_ms) {
           Serial.println("RTCM timeout. Disconnecting...");
           ntripCaster.stop();
           return;
@@ -123,9 +112,13 @@ void beginServer()
         delay(10);
 
         //Report some statistics every 250
-        if (millis() - lastReport_ms > 250)
-        {
-          lastReport_ms += 250;
+        actualReportDelay = millis() - lastReport_ms;
+        if (actualReportDelay > 1000) {
+          ntripCaster.stop();
+          return;
+        }
+        if (actualReportDelay > 250) {
+          lastReport_ms += actualReportDelay;
           Serial.printf("Total sent: %d\n", serverBytesSent);
         }
       }
@@ -136,42 +129,43 @@ void beginServer()
 }
 
 void runNtripServerMode() {
-    pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
-    Serial.begin(115200);
-    delay(250);
+  Serial.begin(115200);
+  delay(250);
 
-    Serial.println();
-    Serial.println("ntrip server mode");
+  Serial.println();
+  Serial.println("ntrip server mode");
 
-    // Connect to WiFi
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(settings.ssid, settings.wifiPassword);
-    unsigned long t0 = millis();
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(300);
-        if (millis() - t0 > 15000) {
-            ESP.restart();
-        }
-        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  // Connect to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(settings.ssid, settings.wifiPassword);
+  unsigned long t0 = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    if (millis() - t0 > 15000) {
+      ESP.restart();
     }
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  }
+  digitalWrite(LED_PIN, LOW);
 
-    Serial.print("WiFi connected. IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.print("WiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
 
-    // Start GNSS UART
-    GNSS.begin(settings.gnssBaud, SERIAL_8N1, UART2_RX, UART2_TX);
+  // Start GNSS UART
+  GNSS.setRxBufferSize(SERIAL_SIZE_RX);
+  GNSS.begin(settings.gnssBaud, SERIAL_8N1, UART2_RX, UART2_TX);
 
-    //Clear any serial characters from the buffer
-    while (Serial.available())
-        Serial.read();
+  //Clear any serial characters from the buffer
+  while (Serial.available())
+    Serial.read();
 
 
-    // Main loop
-    for (;;) {
-        beginServer();
-        delay(1000);
-    }
+  // Main loop
+  for (;;) {
+    beginServer();
+    delay(1000);
+  }
 }
